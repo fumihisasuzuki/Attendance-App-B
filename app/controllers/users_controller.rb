e20d44705283f60e43b31be7621b8cc4ed3a83d1
fumihisasuzuki <fumi_s_qrio192@yahoo.co.jp>
2019-12-05 21:59:24 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
   
   def show
     @user = User.find(params[:id])
@@ -8,4 +20,12 @@ class UsersController < ApplicationController
   
   def edit
   end
+  
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
 end
