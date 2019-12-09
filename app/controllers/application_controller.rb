2ebcdc40c52dfea5f37e755c1a8bb829bc890d01
fumihisasuzuki <fumi_s_qrio192@yahoo.co.jp>
2019-12-09 22:38:06 +0000

    Finish arranging-all-pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,11 +38,11 @@ class ApplicationController < ActionController::Base
     
   # 管理権限者、または現在ログインしているユーザーを許可します。
   def admin_or_correct_user
-    @user = User.find(params[:id]) if @user.blank?
+    @user = params[:user_id].nil? ? User.find(params[:id]) : User.find(params[:user_id])
     unless current_user?(@user) || current_user.admin?
       flash[:danger] = "アクセスするためには本人のログインまたは管理者権限が必要です。"
       redirect_to(root_url)
-    end  
+    end
   end
 
   # ページ出力前に1ヶ月分のデータの存在を確認・セットします。
